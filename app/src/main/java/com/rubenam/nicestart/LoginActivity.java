1139326a31e52fb99a54d1c204191fea800a0b52
Rubén <ru.alba.dev@gmail.com>
2025-12-02 20:37:07 +0100

    Feat/login (#1)
    * feat: create login activity
    
    * feat: add colors to value
    
    * feat: add button styles
    
    * feat: add login gradient
    
    * feat: set login activity layout
    
    * feat: set login activity logic
    
    * feat: add background image to login activity

---
+++ app/src/main/java/com/rubenam/nicestart/LoginActivity.java
@@ -0,0 +1,58 @@
+package com.rubenam.nicestart;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.rubenam.nicestart.databinding.ActivityLoginBinding;
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
+public class LoginActivity extends AppCompatActivity {
+    private ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initListeners();
+
+        Glide.with(this).load("https://i.pinimg.com/736x/be/76/92/be76924d3f0f2007b6804bdf6539fb15.jpg").centerCrop().into(binding.ivLoginBackground);
+    }
+
+    private void initListeners() {
+        // login button
+        binding.btnLoginLogin.setOnClickListener(view -> {
+            String user = binding.etLoginUser.getText().toString().trim();
+            String password = binding.etLoginPassword.getText().toString().trim();
+
+            if (user.isEmpty() || password.isEmpty()) {
+                new SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE)
+                        .setTitleText("Error")
+                        .setContentText("Introduce los datos para logearte")
+                        .show();
+            } else {
+                new SweetAlertDialog(this, SweetAlertDialog.SUCCESS_TYPE)
+                        .setTitleText("Hola " + user)
+                        .setContentText("Logeado correctamente")
+                        .setConfirmText("Continuar")
+                        .setConfirmClickListener(sweetAlertDialog -> {
+                            sweetAlertDialog.dismissWithAnimation();
+
+                            Intent intent = new Intent(this, MainActivity.class);
+                            startActivity(intent);
+                        })
+                        .show();
+            }
+        });
+
+        // sing up button
+        binding.btnLoginSingup.setOnClickListener(view -> {
+            Intent intent = new Intent(this, SingUpActivity.class);
+            startActivity(intent);
+        });
+    }
+}
