cdf8decac2c66ccd503064023fd83e3c9dad3efc
Ruben-am <ru.alba.dev@gmail.com>
2025-12-03 21:07:38 +0100

    feat: set profile activity logic

--- app/src/main/java/com/rubenam/nicestart/ProfileActivity.java
+++ app/src/main/java/com/rubenam/nicestart/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.rubenam.nicestart;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
+import com.rubenam.nicestart.databinding.ActivityProfileBinding;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    private ActivityProfileBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_profile);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        binding = ActivityProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Glide.with(this)
+                .load("https://media.gq.com.mx/photos/61780a08f865d472dfcd66c8/master/w_2560%2Cc_limit/GettyImages-1225777369.jpg")
+                .circleCrop()
+                .into(binding.ivProfileUserpic);
+
+        initListener();
+    }
+
+    private void initListener() {
+        binding.fabProfileAddpic.setOnClickListener(view -> {
+            Toast.makeText(this, "Not yet implemented", Toast.LENGTH_SHORT).show();
         });
     }
 }
