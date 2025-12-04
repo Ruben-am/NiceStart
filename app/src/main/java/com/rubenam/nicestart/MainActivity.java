7e67cfbd7fd74e557e4b4bc478e2b9a6334f8f2e
Rubén <ru.alba.dev@gmail.com>
2025-12-04 11:41:47 +0100

    Feat/main (#7)
    * feat: add swipe refresh to main layout
    
    * feat: add web view to main layout
    
    * feat: add swipe refresh logic in main
    
    * feat: add web view content in main activty
    
    * fix: add flags to login and singup activities

--- app/src/main/java/com/rubenam/nicestart/MainActivity.java
+++ app/src/main/java/com/rubenam/nicestart/MainActivity.java
@@ -2,6 +2,7 @@ package com.rubenam.nicestart;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -11,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.bumptech.glide.Glide;
 import com.rubenam.nicestart.databinding.ActivityLoginBinding;
@@ -27,7 +29,36 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        Glide.with(this).load("https://i.pinimg.com/1200x/a4/f3/ee/a4f3ee9e5e4bdd6222f026a050be49be.jpg").centerCrop().into(binding.ivMainBackground);
+        loadImg();
+
+        binding.srMainRefresh.setOnRefreshListener(mOnRefreshListener);
+        // Glide.with(this).load("https://i.pinimg.com/1200x/a4/f3/ee/a4f3ee9e5e4bdd6222f026a050be49be.jpg").centerCrop().into(binding.ivMainBackground);
+    }
+
+    private void loadImg() {
+        binding.wvMainBkimg.loadDataWithBaseURL(null, genImgString(), "text/html", "UTF-8", null);
+    }
+
+    private String genImgString() {
+
+        String html = "<html>" +
+                "<head><style>" +
+                "html, body { margin:0; padding:0; height:100%; overflow:hidden; }" +
+                "img { width:100%; height:100%; object-fit:cover; }" +
+                "</style></head>" +
+                "<body>" +
+                "<img src='" + imageGen() + "' />" +
+                "</body></html>";
+
+        return html;
+    }
+
+    private String imageGen() {
+        int random = 1 + (int)(Math.random() * 999);
+        String imageUrl = "https://thisbeachdoesnotexist.com/data/seeds-075/" + random + ".jpg";
+        Log.i("imagen", "imageGen: " + random);
+
+        return imageUrl;
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -52,4 +83,18 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    protected SwipeRefreshLayout.OnRefreshListener
+            mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+            Toast toast0 = Toast.makeText(MainActivity.this, "Good job refreshing", Toast.LENGTH_LONG);
+            toast0.show();
+
+
+            binding.wvMainBkimg.reload();
+            loadImg();
+            binding.srMainRefresh.setRefreshing(false);
+        }
+    };
 }
