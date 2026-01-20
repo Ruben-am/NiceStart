150efb196c8c12d0b5160ed08283c1faed004b85
Ruben-am <ru.alba.dev@gmail.com>
2026-01-20 13:32:45 +0100

    add main bab class logic

--- app/src/main/java/com/rubenam/nicestart/MainBab.java
+++ app/src/main/java/com/rubenam/nicestart/MainBab.java
@@ -1,24 +1,57 @@
 package com.rubenam.nicestart;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.rubenam.nicestart.databinding.ActivityMainBabBinding;
 
 public class MainBab extends AppCompatActivity {
+
+    private ActivityMainBabBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main_bab);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        binding = ActivityMainBabBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomAppBar bottomAppBar = binding.babMbMenu;
+        FloatingActionButton myfab = binding.fabMbAdd;
+
+        myfab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainBab.this, "FAB Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainBab.this, "Menu clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.heart_menu_bottom) {
+                    Toast.makeText(MainBab.this, "Added to favourites", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (item.getItemId() == R.id.search_menu_bottom) {
+                    Toast.makeText(MainBab.this, "Beginning search", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            }
         });
     }
 }
