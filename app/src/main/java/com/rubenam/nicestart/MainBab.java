aae22bf4c9a317210050632fe13de9408a0a474f
Ruben-am <ru.alba.dev@gmail.com>
2026-01-29 09:37:09 +0100

    add bottom sheet layout

--- app/src/main/java/com/rubenam/nicestart/MainBab.java
+++ app/src/main/java/com/rubenam/nicestart/MainBab.java
@@ -1,14 +1,17 @@
 package com.rubenam.nicestart;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.rubenam.nicestart.databinding.ActivityMainBabBinding;
 
@@ -23,24 +26,22 @@ public class MainBab extends AppCompatActivity {
         binding = ActivityMainBabBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomAppBar bottomAppBar = binding.babMbMenu;
-        FloatingActionButton myfab = binding.fabMbAdd;
 
-        myfab.setOnClickListener(new View.OnClickListener() {
+        binding.fabMbAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainBab.this, "FAB Clicked", Toast.LENGTH_SHORT).show();
             }
         });
 
-        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+        binding.babMbMenu.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainBab.this, "Menu clicked", Toast.LENGTH_SHORT).show();
+                showBottomSheetDialog();
             }
         });
 
-        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+        binding.babMbMenu.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
                 if (item.getItemId() == R.id.heart_menu_bottom) {
@@ -51,6 +52,42 @@ public class MainBab extends AppCompatActivity {
                     return true;
                 }
                 return false;
+            }
+        });
+    }
+
+    private void showBottomSheetDialog() {
+        View view = LayoutInflater.from(this).inflate(R.layout.bottom_sheet_layout, null);
+
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(view);
+        bottomSheetDialog.show();
+
+        TextView option1 = view.findViewById(R.id.option1);
+        TextView option2 = view.findViewById(R.id.option2);
+        TextView option3 = view.findViewById(R.id.option3);
+
+        option1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainBab.this, "Settings clicked", Toast.LENGTH_SHORT).show();
+                bottomSheetDialog.dismiss();
+            }
+        });
+
+        option2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainBab.this, "About clicked", Toast.LENGTH_SHORT).show();
+                bottomSheetDialog.dismiss();
+            }
+        });
+
+        option3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainBab.this, "Logout clicked", Toast.LENGTH_SHORT).show();
+                bottomSheetDialog.dismiss();
             }
         });
     }
